73f25ae06c62a8bff501abeea44b0f12ca0e75b9
Levan Fuentes-Parra <levanfuentes-parra@Levans-Air.attlocal.net>
2023-04-04 23:53:06 -0700

    Start Project

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest , HttpResponse
 # Create your views here.
+
+def home(request):
+    return render(request , 'home.html')
+def about(request):
+    return render(request , 'home.html')
+def contact(request):
+    return render(request , 'home.html')
+def gallery(request):
+    return render(request , 'home.html')
