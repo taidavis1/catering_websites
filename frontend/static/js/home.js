8c53e3f9611bd318044df32b7280593592066250
Levan Fuentes-Parra <levanfuentes-parra@Levans-MacBook-Air.local>
2023-04-08 02:05:52 -0700

    Navbar done , page1 done

--- frontend/static/js/home.js
+++ frontend/static/js/home.js
@@ -2,3 +2,7 @@ function showNav(){
     document.getElementById("nav").classList.toggle("hidden1");
     document.getElementById("icon").classList.toggle("fa-xmark");
 }
+function down_bg(){
+    document.getElementById("page2").classList.remove("hidden1")
+    document.getElementById("page2").scrollIntoView({behavior: "smooth"});
+}
